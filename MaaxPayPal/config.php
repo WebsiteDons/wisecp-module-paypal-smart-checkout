556b3887b9ed42308ccba8e8c2de6c07fae72191
WebsiteDons <42153624+WebsiteDons@users.noreply.github.com>
2022-11-14 14:10:22 -0500

    Update config.php

--- MaaxPayPal/config.php
+++ MaaxPayPal/config.php
@@ -2,7 +2,10 @@
 return [
     'meta'     => [
         'name'    => '',
-        'version' => '1.0',
+        'version' => '1.0.4',
         'logo'    => 'assets/images/pplogo.png',
-    ]
+    ],
+    'settings' => [
+        'funding_type'        => ['venmo', 'paylater']
+        ]
 ];
